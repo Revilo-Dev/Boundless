8fcbedbe467df5dc2646b7a6e1d6db2ff277f10d
revil <revilo2242@gmail.com>
2026-05-29 18:58:31 +1000

    Update 9.1.1: Assets and final patch

--- src/main/java/net/revilodev/boundless/compat/BoundlessJeiPlugin.java
+++ src/main/java/net/revilodev/boundless/compat/BoundlessJeiPlugin.java
@@ -2,7 +2,4 @@ package net.revilodev.boundless.compat;
 
 import net.minecraft.resources.ResourceLocation;
 
-public final class BoundlessJeiPlugin {
-    private BoundlessJeiPlugin() {}
-    public static final ResourceLocation ID = ResourceLocation.fromNamespaceAndPath("boundless", "jei_plugin");
 }
--- src/main/java/net/revilodev/boundless/compat/LevelUpCompat.java
+++ src/main/java/net/revilodev/boundless/compat/LevelUpCompat.java
@@ -3,7 +3,6 @@ package net.revilodev.boundless.compat;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.fml.ModList;
 
 import java.lang.reflect.Method;
 
